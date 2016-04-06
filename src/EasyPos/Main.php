8ba01c06076a15c3331cace77433d95e7fb756aa
Jake <jakehcasto@aol.com>
2016-04-05 22:09:01 -0400

    Create Main.php

---
+++ src/EasyPos/Main.php
@@ -0,0 +1,35 @@
+<?php
+namespace EasyPos;
+
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\plugin\PluginManager;
+use pocketmine\math\Vector3;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+	$this->saveDefaultConfig();
+    	$this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        $this->getLogger()->info(Color::GREEN ."Enabled!");
+    }
+    
+    public function onDisable(){
+    	$this->getLogger()->info(Color::RED ."Disabled");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if($cmd->getName() == "mypos"){
+        $x = $sender->x;
+        $y = $sender->y;
+        $z = $sender->z;
+        $sender->sendMessage(Color::YELLOW . "Your POS is:\n" . "Your X is: " . Color::GREEN . $x . Color::YELLOW . "\nYour Y is: " . Color::GREEN . $y . Color::YELLOW . "\nYour Z is: " . Color::GREEN . $z . Color::YELLOW . "\nPut your X - Y -Z in the config file to add Floating Text!");
+        }
+}
+}
